d9090ef8100fa525b9f9f512288d7a517ef86640
Aibul <aibulapia@gmail.com>
2026-07-12 22:47:57 +0900

    Add files via upload
    체감온도 계산기 삽입 -안전보건공단 
    단톡카피 .공사안전보고  카피
    체감온도 기록지 구글 spradsheet 실시간 기록-mmeecsafe@

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  supabaseUrl: "https://ojmpeuuldpsfuilnneui.supabase.co",
+  supabaseKey: "sb_publishable_4fsA55-lJq9glbcg9Lqd6A__XmIJwIa",
+  sheetsWebhookUrl: "https://script.google.com/macros/s/AKfycbxPZQ-RW9iDpZaiiT5FaceGlo0v1tbvQMgupstSjTwZAc37Z0eepChi0nAlqgKZKN8n/exec",
+};
